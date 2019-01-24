a141b1bf2f4ba554a0d08a408c84faf4baa9a6ef
Wangsimin0618 <3288540380@qq.com>
2019-01-24 10:13:23 +0800

    6

--- app/src/main/java/demo/com/wdmoviedemo/core/interfase/IRequests.java
+++ app/src/main/java/demo/com/wdmoviedemo/core/interfase/IRequests.java
@@ -20,39 +20,7 @@ import retrofit2.http.Query;
 
 
 public interface IRequests {
-    // 登录
-    @POST("movieApi/user/v1/login")
-    @FormUrlEncoded
-    Observable<Result<LoginData>> login(@Field("phone") String phone, @Field("pwd") String pwd);
-    // 注册
-    @POST("movieApi/user/v1/registerUser")
-    @FormUrlEncoded
-    Observable<Result> register(@Field("nickName") String nickName,
-                                @Field("phone") String phone,
-                                @Field("pwd") String pwd,
-                                @Field("pwd2") String pwd2,
-                                @Field("sex") int sex,
-                                @Field("birthday") String birthday,
-                                @Field("imei") String imei,
-                                @Field("ua") String ua,
-                                @Field("screenSize") String screenSize,
-                                @Field("os") String os,
-                                @Field("email") String email);
-
-    //首页轮播,热门电影
-    @GET("movieApi/movie/v1/findHotMovieList")
-    Observable<Result<List<CarouselData>>> getcarousel(@Header("userid") int userid,
-                                                       @Header("sessionid") String sessionid,
-                                                       @Query("page") int page,
-                                                       @Query("count") int count);
-
-    //首页正在热映
-    @GET("movieApi/movie/v1/findReleaseMovieList")
-    Observable<Result<List<CarouselData>>> getisHit(@Header("userid") int userid,
-                                                    @Header("sessionid") String sessionid,
-                                                    @Query("page") int page,
-                                                    @Query("count") int count);
-    //即将上映
+   //即将上映
     @GET("movieApi/movie/v1/findComingSoonMovieList")
     Observable<Result<List<CarouselData>>> getonNext(@Query("page") int page,
                                                     @Query("count") int count);
