fe4ebcffb4f4619c6140da1d545d04d7809e77a5
Wangsimin0618 <3288540380@qq.com>
2019-01-27 15:01:08 +0800

    1

--- app/src/main/java/demo/com/wdmoviedemo/view/detailsactvity/CheckInActivity.java
+++ app/src/main/java/demo/com/wdmoviedemo/view/detailsactvity/CheckInActivity.java
@@ -52,7 +52,45 @@ public class CheckInActivity extends AppCompatActivity {
     }
 
     private void initSeatTable() {
-        
+        seatView.setScreenName(screeningHall+"荧幕");//设置屏幕名称
+        seatView.setMaxSelected(3);//设置最多选中
+
+        seatView.setSeatChecker(new SeatTable.SeatChecker() {
+
+            @Override
+            public boolean isValidSeat(int row, int column) {
+                if (column == 2) {
+                    return false;
+                }
+                return true;
+            }
+
+            @Override
+            public boolean isSold(int row, int column) {
+                if (row == 6 && column == 6) {
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public void checked(int row, int column) {
+
+            }
+
+            @Override
+            public void unCheck(int row, int column) {
+
+            }
+
+            @Override
+            public String[] checkedSeatTxt(int row, int column) {
+                return null;
+            }
+
+        });
+        seatView.setData(10, 15);
+
     }
 
     private void initData() {
